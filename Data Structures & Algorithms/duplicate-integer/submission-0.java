6c64ff58f52f4a16c394f56f37afa0f37f4f63b5
DEEPAK S <210823104027@kingsedu.ac.in>
2026-05-08 17:19:54 +0530

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        int j=0;
+        int count=0;
+        for(int i=1;i<n;i++){
+            if(i==j){
+                continue;
+            }
+            if(nums[j]==nums[i]){
+                count++;
+            } 
+        }
+        if(count>0){
+            return true
+        } 
+        else{
+            return false
+        }
+    }
+}
